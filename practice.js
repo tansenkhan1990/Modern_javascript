1da028002bfde6f6d61d023eb615c9760275b509
tansen <tansenkhan1990@gmail.com>
2021-07-16 13:02:21 +0200

    add new syntax for arrow functions

--- practice.js
+++ practice.js
@@ -3,10 +3,22 @@ let myFunction = () => {
     return 'this function just has return type'
 }
 
-let myArrowFunction = (name) =>{
+// for one argument return type function
+
+let myFunction3 = age => age * 3 
+
+//for one argument function
+let myArrowFunction = name =>{
+    // console.log(myFunction)
+    console.log(name);
+}
+
+//for multple argument function
+let myArrowFunctionw2 = (name,age) =>{
     // console.log(myFunction)
     console.log(name);
 }
 
 console.log(myFunction())
 myArrowFunction('tansen Khan arrow function')
+console.log(myFunction3(30))
