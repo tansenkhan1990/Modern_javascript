1dd2544f9108aee928d3d73eb31825bf8867f9e4
tansen <tansenkhan1990@gmail.com>
2021-07-16 14:01:35 +0200

    add changes for import and export

---
+++ import.js
@@ -0,0 +1,10 @@
+import { cube, foo, graph } from "export"
+
+graph.options = {
+    color:'blue',
+    thickness:'3px'
+};
+
+graph.draw();
+console.log(cube(3)); // 27
+console.log(foo);    // 4.555806215962888
