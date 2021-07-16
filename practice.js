4fbf30ace264dcd995a83706bffd1abe0b85edda
tansen <tansenkhan1990@gmail.com>
2021-07-16 12:57:57 +0200

    arrow function

---
+++ practice.js
@@ -0,0 +1,12 @@
+let myFunction = () => {
+    // console.log('this is a arrow function');
+    return 'this function just has return type'
+}
+
+let myArrowFunction = (name) =>{
+    // console.log(myFunction)
+    console.log(name);
+}
+
+console.log(myFunction())
+myArrowFunction('tansen Khan arrow function')
